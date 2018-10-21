78b4e32d09d160a34e69dda95245bc1481e08c12
Vito Dierksen <vito@dierksen.me>
2018-10-21 04:04:43 +0200

    random neural net w 0.0% acc. what a succses

---
+++ random.py
@@ -0,0 +1,7 @@
+import tensorflow as ts
+import keras
+import pandas as pd
+import csv
+import numpy as np
+
+data = pd.read_csv('/Users/Vito/Desktop/HackAThon/Trumpf/TRUMPF_TruLaserCenter7030/TRUMPF_TruLaserCenter_Dataset_2018.csv')
